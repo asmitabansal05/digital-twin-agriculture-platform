c9d168d8862322b10e6407a86895326788ba42e2
Asmita Bansal <asmitabansal05@gmail.com>
2026-07-20 22:29:49 +0530

    Fix CORS for Render frontend

--- backend/backend/src/main/java/com/digitaltwin/backend/config/CorsConfig.java
+++ backend/backend/src/main/java/com/digitaltwin/backend/config/CorsConfig.java
@@ -17,7 +17,9 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                        "https://digital-twin-agriculture-platform.onrender.com"
+            )
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
